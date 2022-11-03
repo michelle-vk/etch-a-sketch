cc92dd65a80c0ff0b8ccf560d676afcb275929f3
Michelle <michelle.van.kuijk001@gmail.com>
2022-11-03 10:53:02 +0100

    Set box-sizing on border-box and set for loop which creates grid in a function

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+function createGrid() {
+  const squares = [];
+  for(let i = 0; i < 16; i++) {
+    for(let j = 0; j < 16; j++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      container.appendChild(square);
+      // Push the squares in an array so it can be returned and used in another function
+      squares.push(square);
+    }
+    container.append(document.createElement('br'));
+  }
+  return squares;
+}
+
 const container = document.querySelector(".container");
 
-for(let i = 0; i < 16; i++) {
-  for(let j = 0; j < 16; j++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
-  }
-  container.append(document.createElement('br'));
-}
+createGrid();
